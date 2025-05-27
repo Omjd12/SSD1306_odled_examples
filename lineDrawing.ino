7e66d372a49267bc3568039200f82647861cce1d
Omjd12 <omjadha9728@gmail.com>
2025-05-27 21:16:00 +0530

    Update lineDrawing.ino

--- lineDrawing.ino
+++ lineDrawing.ino
@@ -1,5 +1,8 @@
-#include "SPI.h"
-#include "Wire.h"
+/* in this example we have used adafruit's GFX and SSD1306 library 
+here drawLine function uses the co-ordinates of the end points of the line and color in the integer type */
+
+#include "SPI.h" // used for displays with spi interface
+#include "Wire.h" // usef fot i2c interface 
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
@@ -15,10 +18,13 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  display.drawLine(0,0,display.width(),display.height(),WHITE);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
   for(int i = 0; i<display.width(); i++){
     display.drawLine(0,0,i,display.height(),WHITE); // draws a line with the x,y co-ordinates
-    display.display(); //
+    display.display(); // displays the line
     display.clearDisplay();
   }
   display.clearDisplay();
